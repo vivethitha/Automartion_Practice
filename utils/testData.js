e781a481f96dbe08cccd43fd418378909d29dfa7
vivethitha <vivethitha@example.com>
2026-09-23 17:05:34 +0530

    nw

--- utils/testData.js
+++ utils/testData.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const { parse } = require('csv-parse/sync');
+//const { parse } = require('csv-parse/sync');
 
 //function loadJson(fileName) {
   const filePath = path.join(__dirname, '..', 'data', fileName);
   return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 //}
 
-function loadCsv(fileName) {
+//function loadCsv(fileName) {
   const filePath = path.join(__dirname, '..', 'data', fileName);
   const csv = fs.readFileSync(filePath, 'utf8');
   return parse(csv, { columns: true, skip_empty_lines: true, trim: true });
-}
+//}
 
 module.exports = { loadJson, loadCsv };
