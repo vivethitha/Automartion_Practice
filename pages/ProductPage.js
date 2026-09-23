d2bcbee96326394f68ade0a03f5d12fdfa764713
vivethitha <vivethithad@gmail.com>
2026-09-23 16:13:08 +0530

    New changes

--- pages/ProductPage.js
+++ pages/ProductPage.js
@@ -1,7 +1,7 @@
 const { expect } = require('@playwright/test');
 const locators = require('../locators/amazon.locators');
 
-//class ProductPage {
+class ProductPage {
   constructor(page) {
     this.page = page;
     this.productResults = page.locator(locators.product.productResults);
@@ -12,11 +12,11 @@ const locators = require('../locators/amazon.locators');
   async openProduct(productName = '') {
     await this.productResults.first().waitFor({ state: 'visible', timeout: 5000 });
     await this.productResults.first().click();
-    //await this.page.waitForURL(/\/dp\//, { timeout: 10000 });
+    await this.page.waitForURL(/\/dp\//, { timeout: 10000 });
   }
 
   async addToCart() {
-    //await this.addToCartButton.waitFor({ state: 'visible', timeout: 8000 });
+    await this.addToCartButton.waitFor({ state: 'visible', timeout: 8000 });
     await this.addToCartButton.scrollIntoViewIfNeeded();
     await this.addToCartButton.click();
     await this.page.getByText(/Added to Cart|Added to cart|Cart/i).waitFor({ state: 'visible', timeout: 8000 });
